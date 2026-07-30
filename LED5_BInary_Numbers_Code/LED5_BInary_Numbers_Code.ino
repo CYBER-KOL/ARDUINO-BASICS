71a08a8bcf47fa0e9e1ed02b8a06aace3346b6f4
KOL <gkkaleacoba@gmail.com>
2026-07-30 16:56:19 +0800

    Add files via upload

---
+++ LED5_BInary_Numbers_Code/LED5_BInary_Numbers_Code.ino
@@ -0,0 +1,277 @@
+int yellow=11;
+int H=HIGH;
+int L=LOW;
+int green=12;
+int blue=13;
+int red=10;
+int orange=9;
+float pi=3.14;
+int da=500;
+int Long=1000;
+
+String myName="Godwin Kale";
+void setup() {
+
+  // put your setup code here, to run once:
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(orange, OUTPUT);
+}
+
+void loop() {
+  
+  // 0
+digitalWrite(blue, L);
+digitalWrite(green, L);
+digitalWrite(yellow, L);
+digitalWrite(red, L);
+digitalWrite(orange, L);
+
+
+ delay(Long);
+
+//1
+digitalWrite(blue, H);
+digitalWrite(green, L);
+digitalWrite(yellow, L);
+ digitalWrite(red, L);
+digitalWrite(orange, L);
+ delay(Long);
+
+ //2
+digitalWrite(blue, L);
+digitalWrite(green, H);
+digitalWrite(yellow, L);
+digitalWrite(red, L);
+digitalWrite(orange, L);
+ delay(Long);
+
+//3
+ digitalWrite(blue, H);
+digitalWrite(green, H);
+digitalWrite(yellow, L);
+digitalWrite(red, L);
+digitalWrite(orange, L);
+ delay(Long);
+ 
+ //4
+ digitalWrite(blue, L);
+digitalWrite(green, L);
+digitalWrite(yellow, H);
+digitalWrite(red, L);
+digitalWrite(orange, L);
+ delay(Long);
+
+
+//5
+ digitalWrite(blue, H);
+digitalWrite(green, L);
+digitalWrite(yellow, H);
+digitalWrite(red, L);
+digitalWrite(orange, L);
+ delay(Long);
+
+ //6
+ digitalWrite(blue, L);
+digitalWrite(green, H);
+digitalWrite(yellow, H);
+ digitalWrite(red, L);
+digitalWrite(orange, L);
+ delay(Long);
+
+ //7
+ digitalWrite(blue, H);
+digitalWrite(green, H);
+digitalWrite(yellow, H);
+ digitalWrite(red, L);
+ digitalWrite(orange, L);
+
+ delay(Long);
+
+//8
+ digitalWrite(blue, L);
+digitalWrite(green, L);
+digitalWrite(yellow, L);
+ digitalWrite(red, H);
+digitalWrite(orange, L);
+ delay(Long);
+
+ //9
+ digitalWrite(blue, H);
+digitalWrite(green, L);
+digitalWrite(yellow, L);
+ digitalWrite(red, H);
+digitalWrite(orange, L);
+ delay(Long);
+
+ //10
+ digitalWrite(blue, L);
+digitalWrite(green, H);
+digitalWrite(yellow, L);
+ digitalWrite(red, H);
+digitalWrite(orange, L);
+ delay(Long);
+
+ //11
+ digitalWrite(blue, H);
+digitalWrite(green, H);
+digitalWrite(yellow, L);
+ digitalWrite(red, H);
+digitalWrite(orange, L);
+ delay(Long);
+
+ //12
+ digitalWrite(blue, L);
+digitalWrite(green, L);
+digitalWrite(yellow, H);
+ digitalWrite(red, H);
+digitalWrite(orange, L);
+ delay(Long);
+
+ //13
+ digitalWrite(blue, H);
+ digitalWrite(green, L);
+digitalWrite(yellow, H);
+digitalWrite(red, H);
+digitalWrite(orange, L);
+delay(Long);
+
+//14
+digitalWrite(blue, L);
+digitalWrite(green, H);
+digitalWrite(yellow, H);
+digitalWrite(red, H);
+digitalWrite(orange, L);
+delay(Long);
+
+//15
+digitalWrite(blue, H);
+digitalWrite(green, H);
+digitalWrite(yellow, H);
+digitalWrite(red, H);
+digitalWrite(orange, L);
+delay(Long);
+
+//16
+digitalWrite(blue, L);
+digitalWrite(green, L);
+digitalWrite(yellow, L);
+digitalWrite(red, L);
+digitalWrite(orange, H);
+delay(Long);
+
+//17
+digitalWrite(blue, H);
+digitalWrite(green, L);
+digitalWrite(yellow, L);
+digitalWrite(red, L);
+digitalWrite(orange, H);
+delay(Long);
+//18
+digitalWrite(blue, L);
+digitalWrite(green, H);
+digitalWrite(yellow, L);
+digitalWrite(red, L);
+digitalWrite(orange, H);
+delay(Long);
+//19
+digitalWrite(blue, H);
+digitalWrite(green, H);
+digitalWrite(yellow, L);
+digitalWrite(red, L);
+digitalWrite(orange, H);
+delay(Long);
+//20
+digitalWrite(blue, L);
+digitalWrite(green, L);
+digitalWrite(yellow, H);
+digitalWrite(red, L);
+digitalWrite(orange, H);
+delay(Long);
+//21
+digitalWrite(blue, H);
+digitalWrite(green, L);
+digitalWrite(yellow, H);
+digitalWrite(red, L);
+digitalWrite(orange, H);
+delay(Long);
+//22
+digitalWrite(blue, L);
+digitalWrite(green, H);
+digitalWrite(yellow, H);
+digitalWrite(red, L);
+digitalWrite(orange, H);
+delay(Long);
+//23
+digitalWrite(blue, H);
+digitalWrite(green, H);
+digitalWrite(yellow, H);
+digitalWrite(red, L);
+digitalWrite(orange, H);
+delay(Long);
+//24
+digitalWrite(blue, L);
+digitalWrite(green, L);
+digitalWrite(yellow, L);
+digitalWrite(red, H);
+digitalWrite(orange, H);
+delay(Long);
+//25
+digitalWrite(blue, H);
+digitalWrite(green, L);
+digitalWrite(yellow, L);
+digitalWrite(red, H);
+digitalWrite(orange, H);
+delay(Long);
+
+//26
+digitalWrite(blue, L);
+digitalWrite(green, H);
+digitalWrite(yellow,L);
+digitalWrite(red, H);
+digitalWrite(orange, H);
+delay(Long);
+
+//27
+digitalWrite(blue, H);
+digitalWrite(green, H);
+digitalWrite(yellow, L);
+digitalWrite(red, H);
+digitalWrite(orange, H);
+delay(Long);
+
+//28
+digitalWrite(blue, L);
+digitalWrite(green, L);
+digitalWrite(yellow, H);
+digitalWrite(red, H);
+digitalWrite(orange, H);
+delay(Long);
+
+//29
+digitalWrite(blue, H);
+digitalWrite(green, L);
+digitalWrite(yellow, H);
+digitalWrite(red, H);
+digitalWrite(orange, H);
+delay(Long);
+
+//30
+digitalWrite(blue, L);
+digitalWrite(green, H);
+digitalWrite(yellow, H);
+digitalWrite(red, H);
+digitalWrite(orange, H);
+delay(Long);
+
+//31
+digitalWrite(blue, H);
+digitalWrite(green, H);
+digitalWrite(yellow, H);
+digitalWrite(red, H);
+digitalWrite(orange, H);
+delay(Long);
+
+}
